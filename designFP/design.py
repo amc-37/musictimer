563cf75960d5ca2a6dcc17734c4ffa3e0853566e
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-06 20:40:30 +0000

    Mon, Apr 6, 2026, 4:40 PM -04:00

--- designFP/design.py
+++ designFP/design.py
@@ -2,4 +2,18 @@
 import datetime
 import radiohead.json as rh
 
+def songs_path():
+    """return the path to a spotify playlist in the form of """
+    # for now, use radiohead.json to find path
+    pass
 
+
+def main():
+    # call function retrieving user input for spotify path to playlist
+    playlist = songs_path()
+
+    #
+
+
+if __name__ == '__main__':
+    main()

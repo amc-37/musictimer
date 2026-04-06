a415d4f02e2b2d01a5bd7c7398e21c985154ac63
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-06 20:54:29 +0000

    Mon, Apr 6, 2026, 4:54 PM -04:00

--- designFP/design.py
+++ designFP/design.py
@@ -3,24 +3,32 @@ import datetime
 import radiohead.json as rh
 
 def songs_path(inp):
-    """ask for input in the form of a url, and return the path to a spotify playlist in the form of a list of songs in the playlist"""
+    """ask for input in the form of a url, and return
+    the path to a spotify playlist in the form of a list of songs in the playlist"""
     # ask for input in the form of a url playlist link, and check that this is valid
     # for now, use radiohead.json to find path
     pass #maybe hard code this in for now
     return q
 
 def stop_type(inp):
+    """given an input time, duration, or songs, determine either a time,
+    a duration, or a number of songs and return it using datetime library"""
     pass
-    return 
+    return t, d, s
 
 def main():
     # call function retrieving user input for spotify path to playlist
     playlist = input("enter your playlist") # whatever we want
     queue = songs_path(playlist)
 
+    # call function asking for input about time, duration, or songs
+    type = input("do you want to stop after time, duration, or songs?")
+    time, duration, songs = stop_type(type)
+    # do work with time
 
-    # call function asking for input about duration, songs, or time
+    # do work with duration
 
+    # do work with songs
 
 if __name__ == '__main__':
     main()

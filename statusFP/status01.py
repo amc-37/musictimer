de5ee84c8efcbc4f619cae188a00b90466dc4a53
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-19 02:25:18 +0000

    Sat, Apr 18, 2026, 10:25 PM -04:00

--- statusFP/status01.py
+++ statusFP/status01.py
@@ -7,15 +7,6 @@ from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-        scope="user-modify-playback-state user-read-playback-state playlist-read-private"
-    )
-)
-
 SCOPES = "user-modify-playback-state user-read-playback-state playlist-read-private"
 
 def get_spotify_client():

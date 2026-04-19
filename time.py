12af1dd613e3dc310a76d28d5ea5a74ffd707ca4
amc-37 <ariannamarcav@gmail.com>
2026-04-19 02:30:55 +0000

    Sat, Apr 18, 2026, 10:30 PM -04:00

--- time.py
+++ time.py
@@ -1,2 +1,28 @@
 import time
 
+def wait_then_stop(duration_seconds):
+    """Wait for duration_seconds, then stop."""
+    print("Starting...")
+    start = time.perf_counter()
+
+    # Wait until the elapsed time reaches the duration
+    while True:
+        elapsed = time.perf_counter() - start
+        if elapsed >= duration_seconds:
+            break
+        # Optional: print progress
+        # print(f"Elapsed: {elapsed:.1f} s", end="\r")
+        time.sleep(0.1)  # small sleep so we don't burn CPU
+
+    print("Time is up! Stopping now.")
+
+def main():
+
+    min = int(input('please give a number of minutes'))
+    sec = min*60
+    print(sec)
+
+    wait_then_stop(sec)
+
+if __name__ == '__main__':
+    main()

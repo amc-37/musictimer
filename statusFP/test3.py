9851b6874d99013fc16d372ef547a248198fa394
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-21 18:33:01 +0000

    Tue, Apr 21, 2026, 2:33 PM -04:00

---
+++ statusFP/test3.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        scope="user-read-email",
+        open_browser=True,
+    )
+)
+
+print(sp.current_user())

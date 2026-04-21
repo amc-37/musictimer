a44e582c9eecf2d88052a149d036804670bd099b
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-21 18:41:49 +0000

    Tue, Apr 21, 2026, 2:41 PM -04:00

--- statusFP/test.py
+++ statusFP/test.py
@@ -1,11 +1,14 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-from pathlib import Path
-load_dotenv(Path(__file__).resolve().parent.parent / ".env") # it doesn't matter what folder .env file is in now
+load_dotenv(Path(__file__).resolve().parent.parent / ".env")
+
 print("CLIENT_ID:", os.getenv("SPOTIPY_CLIENT_ID"))
+print("CLIENT_SECRET exists:", os.getenv("SPOTIPY_CLIENT_SECRET") is not None)
+print("REDIRECT_URI:", repr(os.getenv("SPOTIPY_REDIRECT_URI")))
 
 REDIRECT = "http://127.0.0.1:8888/callback"
 print("CLIENT_ID:", os.getenv("SPOTIPY_CLIENT_ID"))

8939dacbc1ee89fb31c3e5adbc8c6ae028acf89b
avizimmerman1128 <avi.zimmerman82@gmail.com>
2026-04-22 00:56:56 +0000

    Tue, Apr 21, 2026, 8:56 PM -04:00

--- statusFP/status02.py
+++ statusFP/status02.py
@@ -58,7 +58,7 @@ def stop_at_clock_time(sp, device_id, stop_time_str):
     seconds = (stop_dt - now).total_seconds()
     # Compute seconds until target
     seconds_to_target = (stop_dt - now).total_seconds()
-    print(f"Will stop in {seconds_to_target:.0f} seconds (at {target_dt.time()}).")
+    print(f"Will stop in {seconds_to_target:.0f} seconds (at {stop_dt.time()}).")
     time.sleep(seconds)
     pause(sp, device_id)
 
